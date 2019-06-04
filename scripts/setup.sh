8bfaf831ca1208c68d3d94f8cc200dad3682040c
MaxenceMottard <mottard.maxence@gmail.com>
2019-06-04 11:55:26 +0200

    Move scripts into a directory

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
 echo deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main > /etc/apt/sources.list.d/pgdg.list
 
+# Import the
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-apt-get update
-apt-get install postgresql-11
+
+#
+apt-get install postgresql-11 -y
