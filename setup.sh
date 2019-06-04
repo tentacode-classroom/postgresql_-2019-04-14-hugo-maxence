c9ef3a1101ae6d59f06c50c7ff687fcf17521a59
Hugo <hugo.bordais@ynov.com>
2019-06-04 12:06:51 +0200

    add -> create_event_database.sh / Modify -> setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 echo deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main > /etc/apt/sources.list.d/pgdg.list
 
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
