dec345aa15dec3775a58c0bb214c7ba315819786
MaxenceMottard <mottard.maxence@gmail.com>
2019-06-04 12:50:14 +0200

    Fix create_database script

--- scripts/create_event_database.sh
+++ scripts/create_event_database.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+# Set variables
+FILE_DIRECTORY=`dirname $0`
+PROJECT_DIRECTORY=$FILE_DIRECTORY/..
+
+# Create tables in database
+sudo -u postgres psql -f $PROJECT_DIRECTORY/sql/databaseManager.sql
+echo '[ INFO ] Lancement du fichier databaseManager.sql'
