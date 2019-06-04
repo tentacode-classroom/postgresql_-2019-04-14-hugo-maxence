ff71f5f82939d77dc6c0b3a540e339963954863f
MaxenceMottard <mottard.maxence@gmail.com>
2019-06-04 12:18:40 +0200

    Comment setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
+# Add the repository in sources
 echo deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main > /etc/apt/sources.list.d/pgdg.list
 
-# Import the
+# Import the repository
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
-#
+# Install the package
 apt-get install postgresql-11 -y
+
+
