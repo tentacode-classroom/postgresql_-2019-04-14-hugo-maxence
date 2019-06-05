ba43ead405dbc36c531e213bbc4127bc6dc1559b
Hugo <hugo.bordais@ynov.com>
2019-06-05 12:48:39 +0200

    Edit -> change type (id), change values on Insert

--- sql/create_event_database.sql
+++ sql/create_event_database.sql
@@ -3,28 +3,30 @@
 DROP DATABASE IF EXISTS github_events;
 CREATE DATABASE github_events;
 
+\c github_events;
+
 -- Create table with values
 
 CREATE TABLE repo
 (
-    id    INT PRIMARY KEY NOT NULL,
-    login VARCHAR(255)    NOT NULL,
-    name  VARCHAR(255)    NOT NULL,
-    url   VARCHAR(255)    NOT NULL
+    id    SERIAL PRIMARY KEY NOT NULL,
+    login VARCHAR(255)       NOT NULL,
+    name  VARCHAR(255)       NOT NULL,
+    url   VARCHAR(255)       NOT NULL
 );
 
 CREATE TABLE actor
 (
-    id            INT PRIMARY KEY NOT NULL,
-    login         VARCHAR(255)    NOT NULL,
-    display_login VARCHAR(255)    NOT NULL,
-    url           VARCHAR(255)    NOT NULL,
+    id            SERIAL PRIMARY KEY NOT NULL,
+    login         VARCHAR(255)       NOT NULL,
+    display_login VARCHAR(255)       NOT NULL,
+    url           VARCHAR(255)       NOT NULL,
     avatar_url    VARCHAR(255)
 );
 
 CREATE TABLE events
 (
-    id     INT PRIMARY KEY           NOT NULL,
+    id     SERIAL PRIMARY KEY        NOT NULL,
     actor  INT REFERENCES actor (id) NOT NULL,
     repo   INT REFERENCES repo (id)  NOT NULL,
     public boolean                   NOT NULL
@@ -42,31 +44,37 @@ CREATE TABLE push_event
 
 -- Insert fake values on tables to test
 
-INSERT INTO repo (id, login, name, url)
-VALUES ('HG120', 'repo-max', 'maxence', 'https://github.com/MaxenceMottard'),
-       ('T_601', 'repo-hugo', 'hugo', 'https://github.com/HugoBordais');
+INSERT INTO repo (login, name, url)
+VALUES ('repo-max', 'maxence', 'https://github.com/MaxenceMottard'),
+       ('repo-hugo', 'hugo', 'https://github.com/HugoBordais');
 
-INSERT INTO actor (id, login, display_login, url, avatar_url)
-VALUES ('UA502', 'max', 'max', 'https://github.com/MaxenceMottard',
+INSERT INTO actor (login, display_login, url, avatar_url)
+VALUES ('max', 'max', 'https://github.com/MaxenceMottard',
         'https://avatars1.githubusercontent.com/u/28975415?s=400&v=4'),
-       ('B6717', 'hugo', 'hugo', 'https://github.com/HugoBordais',
+       ('hugo', 'hugo', 'https://github.com/HugoBordais',
         'https://avatars3.githubusercontent.com/u/33489155?s=400&v=4');
 
-INSERT INTO events (id, actor, repo, public)
-VALUES ('PH917', 'jean-paul', 'azertyuiop', true),
-       ('MH420', 'pierre-henry', 'poiuytreza', true);
+INSERT INTO events (actor, repo, public)
+VALUES (1, 2, true),
+       (2, 1, true);
 
-INSERT INTO issue_event (id, actor, repo, public, issue_url)
-VALUES ('PH7MH91420', 'jean-paul', 'azertyuiop', true, 'https://github.com/MaxenceMottard'),
-       ('B6UA571702', 'pierre-henry', 'poiuytreza', true, 'https://github.com/HugoBordais');
+INSERT INTO issue_event (actor, repo, public, issue_url)
+VALUES (2, 2, true, 'https://github.com/MaxenceMottard'),
+       (1, 2, true, 'https://github.com/HugoBordais');
 
-INSERT INTO push_event (id, actor, repo, public, commits)
-VALUES ('PH7MHHG12091420', 'jean-paul', 'azertyuiop', true, 37),
-       ('B6UAT_601571702', 'pierre-henry', 'poiuytreza', true, 0);
+INSERT INTO push_event (actor, repo, public, commits)
+VALUES (1, 1, true, 37),
+       (2, 1, true, 0);
 
 -- Show tables
 
-SELECT * FROM repo;
-SELECT * FROM actor;
-SELECT * FROM issue_event;
-SELECT * FROM push_event;
+SELECT *
+FROM repo;
+SELECT *
+FROM actor;
+SELECT *
+FROM events;
+SELECT *
+FROM issue_event;
+SELECT *
+FROM push_event;
