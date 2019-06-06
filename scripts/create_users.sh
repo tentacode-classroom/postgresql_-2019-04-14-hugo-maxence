900e06baf35c0092d601a78237f74d9f67ea27b9
Hugo <hugo.bordais@ynov.com>
2019-06-06 15:15:45 +0200

    Edit -> change location on "echo [ INFO ]"

--- scripts/create_users.sh
+++ scripts/create_users.sh
@@ -5,9 +5,9 @@ FILE_DIRECTORY=`dirname $0`
 PROJECT_DIRECTORY=$FILE_DIRECTORY/..
 
 # Create the super_user user
-sudo -u postgres psql -f $PROJECT_DIRECTORY/sql/create_super_user.sql
 echo '[ INFO ] Lancement du fichier create_super_user.sql'
+sudo -u postgres psql -f $PROJECT_DIRECTORY/sql/create_super_user.sql
 
 # Create the github_events user
-sudo -u postgres psql -f $PROJECT_DIRECTORY/sql/create_github_user.sql
 echo '[ INFO ] Lancement du fichier create_github_user.sql'
+sudo -u postgres psql -f $PROJECT_DIRECTORY/sql/create_github_user.sql
