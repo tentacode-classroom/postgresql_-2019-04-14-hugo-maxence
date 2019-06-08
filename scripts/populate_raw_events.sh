d0555c6da979f474d1b1f917c2bd507a74323d5b
MaxenceMottard <mottard.maxence@gmail.com>
2019-06-08 13:02:36 +0200

    Add info for populate_raw_events script

--- scripts/populate_raw_events.sh
+++ scripts/populate_raw_events.sh
@@ -8,13 +8,13 @@ echo '[ INFO ] Création de la table events_raw'
 psql --username super_admin -d github_events -f $PROJECT_DIRECTORY/sql/create_events_raw_table.sql
 
 
-echo '[ INFO ] Création des fichiers SQL'
 LINE_NUMBER=1
-
 rm -rf $PROJECT_DIRECTORY/data.sql
 
 QUERY="INSERT INTO events_raw (data_json) values"
 
+echo '[ INFO ] Insertion des lignes dans la base de données.'
+echo '[ WARNING ] Ça peut-être très long 🙄'
 cat $PROJECT_DIRECTORY/data.json | sed 's/\\/\\\\/g' | while read LINE ; do
 #       POUR GABRIEL :  Le problème des escape venait du read juste au dessus !!!!
     LINE=`echo $LINE | sed s/\'/#/g`
@@ -23,8 +23,10 @@ cat $PROJECT_DIRECTORY/data.json | sed 's/\\/\\\\/g' | while read LINE ; do
     then
         QUERY="$QUERY ('$LINE');"
         echo $QUERY > $PROJECT_DIRECTORY/data.sql
-        QUERY="INSERT INTO events_raw (data_json) values"
         psql --username super_admin -d github_events -f $PROJECT_DIRECTORY/data.sql
+        echo "[ INFO ] 1000 lignes viennent d'être insérées en base de données."
+
+        QUERY="INSERT INTO events_raw (data_json) values"
         LINE_NUMBER=1
     else
         ((LINE_NUMBER+=1))
@@ -36,5 +38,7 @@ done
 QUERY="$QUERY ('$LINE');"
 echo $QUERY > $PROJECT_DIRECTORY/data.sql
 psql --username super_admin -d github_events -f $PROJECT_DIRECTORY/data.sql
+
+echo "[ INFO ] Insertion terminée"
 rm $PROJECT_DIRECTORY/data.sql
 
