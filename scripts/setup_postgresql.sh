a190594b8478bf36649d6049e7e7128b63582b2c
MaxenceMottard <mottard.maxence@gmail.com>
2019-06-16 12:08:50 +0200

    Add check version after setup

--- scripts/setup_postgresql.sh
+++ scripts/setup_postgresql.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-# Add the repository in sources
-echo "[ INFO ] Ajout du repository dans la sources.list"
+echo "[ INFO ] Ajout du repository dans le ficher sources.list"
 echo "deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main" | sudo tee --append /etc/apt/sources.list.d/pgdg.list
 
-
-# Import the repository
 echo "[ INFO ] Import du repository"
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add
 sudo apt-get update
 
-# Install the package
 echo "[ INFO ] Lancement de l'installation de PostgreSQL version 11"
-apt-get install postgresql-11 -y
+sudo apt-get install postgresql-11 -y
 
-# Start postgres
 echo "[ INFO ] Démarrage du service PostgreSQL"
 sudo service postgresql start
+
+echo "[ INFO ] Version : "
+sudo -u postgres psql --version
