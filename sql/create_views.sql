b7b7ba58369366275a6f6df6a7a5f0cc64bc682b
Hugo <hugo.bordais@ynov.com>
2019-06-16 16:49:45 +0200

    ORDER BY in Views

--- sql/create_views.sql
+++ sql/create_views.sql
@@ -3,11 +3,12 @@
 DROP VIEW IF EXISTS push_event_count_commits;
 
 CREATE VIEW push_event_count_commits AS
-SELECT SUM(pe.commits),
-       repo.name
+SELECT SUM(pe.commits) number_commits,
+       repo.name       repo_name
 FROM push_event pe
          JOIN repo ON repo.id = pe.repo
-GROUP BY repo.id;
+GROUP BY repo.id
+ORDER BY number_commits;
 
 
 SELECT *
